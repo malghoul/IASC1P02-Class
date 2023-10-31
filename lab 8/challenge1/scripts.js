d6dca45416fce304363776cbf05b69f1bc1836d5
malghoul <alghoulmimi@gmail.com>
2023-10-31 13:18:36 -0400

    lab 8 challenge 1 html and js

--- lab 8/challenge1/scripts.js
+++ lab 8/challenge1/scripts.js
@@ -1,7 +1,6 @@
-/A Function to printout Hurrah! when called
+//A Function to printout Hurrah! when called
 
-function pushMe(){
-	document.getElementBy("output").innerHTML="Hurrah!";
+function pushMe()
+{
+	document.getElementById("output").innerHTML="Hurrah!";
 }
-
-
